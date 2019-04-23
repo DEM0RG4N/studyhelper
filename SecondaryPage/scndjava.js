1b7948c37e8216d901b4a5a60fb899e366ebd00c
BekkaliyevAssan <marina.neymar.13@gmail.com>
2019-04-23 21:45:39 +0600

    old fieles

--- SecondaryPage/scndjava.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-        $(".list-unstyled a").eq(2).addClass('active');
-    var topic = $(".list-unstyled .active").text();
-    $(".lessonTopic").text(topic);
-
-});
--- personalAccount/scriptPersonalAccount.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$("#addNewCourse").click(function(){
-		
-		$("ul").append("<li>hello</li>");
-	});
-});
